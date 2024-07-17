e199974e9a7a481a0edf37df8a2b44a816ed0822
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-07-18 00:22:48 +0530

    17july - day3

--- 20. Valid Parentheses.cc
+++ 20. Valid Parentheses.cc
@@ -9,15 +9,13 @@ public:
     stack<char>stk;
     
     for(char ch:s){
-        if(ch=='(' || ch=='{' || ch=='[') stk.push(ch);
-        else{
-            if(stk.empty()) return false;
-            if(ch==')' && stk.top()=='(') stk.pop(); //pair hogi toh stk khali karo, ni h toh false
-            else  if(ch==']' && stk.top()=='[') stk.pop();
-            else  if(ch=='}' && stk.top()=='{') stk.pop();
-            else return false;
-        }
+    if(ch=='(' || ch=='{' || ch=='[') stk.push(ch);
+     else{
+        if(stk.empty() || abs(ch-stk.top())>2 ) return false; //abs() ASCII value chk karta h
+        //parenthesis ke values mei 1-2 ka fark hota h
+        else stk.pop(); //else toh pair thik h hogi 100%, isiliye stk khali karo
     }
-    return stk.empty();
+    }
+    return stk.empty(); //stk poora khali ho toh for sure sab pairs thik the
     }
 };
